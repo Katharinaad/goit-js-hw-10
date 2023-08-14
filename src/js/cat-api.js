d3a93144120b96bcb11464f9fe251621920e3418
Katharinaad <vermilion.adams@gmail.com>
2023-08-14 23:33:31 +0200

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -4,27 +4,54 @@ const API_KEY =
   live_GMXh4r5NDbNPMNWdeeLaNoIHSSjWhUpHXcLKejzKMQ9stY6OSSU7cY7xjFVthT7A;
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds';
-const SEARCH_URL = 'https://api.thecatapi.com/v1/images/search';
-
 function fetchBreeds() {
-  return axios({
-    method: 'get',
-    url: BREEDS_URL,
-  })
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
-}
+  const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds';
 
-console.log(fetchBreeds());
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+  });
+
+  return fetch(`${BREEDS_URL}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
 
 function fetchCatByBreed(breedId) {
-  return axios({
-    method: 'get',
-    url: SEARCH_URL,
-  })
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+  const SEARCH_URL = 'https://api.thecatapi.com/v1/images/search';
+
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+  });
+
+  return fetch(`${SEARCH_URL}/${breedId}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
 
-console.log(fetchCatByBreed(breedId));
+// function fetchBreeds() {
+//   return axios({
+//     method: 'get',
+//     url: BREEDS_URL,
+//   })
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error));
+// }
+
+// console.log(fetchBreeds());
+
+// function fetchCatByBreed(breedId) {
+//   return axios({
+//     method: 'get',
+//     url: SEARCH_URL,
+//   })
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error));
+// }
+
+// console.log(fetchCatByBreed(breedId));
