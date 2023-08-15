9e8d1b59031c17350b707d4c0b000811a0fcbc09
Katharinaad <vermilion.adams@gmail.com>
2023-08-15 13:11:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './css/styles.css';
 import { fetchCatByBreed } from './js/cat-api';
+import { fetchBreeds } from './js/cat-api';
+
+import Notiflix from 'notiflix';
 
 const selectBreed = document.querySelector('.breed-select');
-const catDescription = document.querySelector('.cat-info');
+const catPicture = document.querySelector('.cat-picture');
+const catDescription = document.querySelector('.cat-description');
 const loaderElement = document.querySelector('.loader');
 
 selectBreed.addEventListener('change', changeCatBreed);
@@ -11,4 +15,29 @@ function changeCatBreed(event) {
   const breedId = event.target.value;
   console.log('breedId: ', breedId);
   fetchCatByBreed(breedId);
+}
+
+function fetchAndRenderBreeds() {
+  loaderElement.classList.remove('hidden');
+  fetchBreeds()
+    // .then(breeds => console.log(breeds))
+    .then(breeds => renderBreedsSelect(breeds))
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => loaderElement.classList.add('hidden'));
+}
+
+fetchAndRenderBreeds();
+
+function renderBreedsSelect(breeds) {
+  const optionMarkup = breeds
+    .map(breed => {
+      return `<option value="${breed.reference_image_id}">${breed.name}</option>`;
+    })
+    .join('');
+  selectBreed.insertAdjacentHTML('beforeend', optionMarkup);
 }
