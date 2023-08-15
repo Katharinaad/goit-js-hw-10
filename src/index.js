117c588c9d90b005ea61fa77b78332a9114196e6
Katharinaad <vermilion.adams@gmail.com>
2023-08-15 11:04:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
+import { fetchCatByBreed } from './js/cat-api';
 
 const selectBreed = document.querySelector('.breed-select');
+const catDescription = document.querySelector('.cat-info');
+const loaderElement = document.querySelector('.loader');
 
-// const breedId = event.target.value;
+selectBreed.addEventListener('change', changeCatBreed);
+
+function changeCatBreed(event) {
+  const breedId = event.target.value;
+  console.log('breedId: ', breedId);
+  fetchCatByBreed(breedId);
+}
